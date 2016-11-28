7d7bdda3987818e7e47822e0c3441908bf3ea81b
Biddrup Kumar Mallick <Biddrup@users.noreply.github.com>
2016-11-28 22:42:50 +0600

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,16 @@
+#Will make a list of your given valu
+d = []
+
+#Will sum up
+c = 0
+while True:
+    print('Give me a number')
+    a = input()
+    if a == '':
+        break
+    else:
+        b = int(a)
+        c = c + b
+        d.append(b)
+print('You have putted ' + d)
+print('And, sum of them 'c)
